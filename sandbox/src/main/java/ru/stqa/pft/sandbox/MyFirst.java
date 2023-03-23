4edbb7369d51faae9bcd74e349f7cd8cb277727d
migushova <ned-ns@bk.ru>
2023-03-23 22:53:06 +0300

    добавление тестов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirst.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirst.java
@@ -4,6 +4,16 @@ public class MyFirst {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника = " + r.area());
+
+        Point p = new Point(1, 5);
+        System.out.println("Расстояние равно =" + p.distance());
     }
-}
+
+
+    };
+
